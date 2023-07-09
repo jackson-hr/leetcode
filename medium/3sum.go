a35bcb49b11fa15315c682fb43e89a38581500be
jackson-hr <jackson_hr@yeah.net>
2023-07-09 23:27:04 +0800

    3sum optimize

--- medium/3sum.go
+++ medium/3sum.go
@@ -7,20 +7,29 @@ func threeSum(nums []int) [][]int {
 	n := len(nums)
 
 	res := make([][]int, 0, 4)
-	for l := 0; l < n; l++ {
+	for l := 0; l < n-2; l++ {
 		// skip repeat
 		if l > 0 && nums[l] == nums[l-1] {
+			continue
+		}
+
+		if nums[l]+nums[n-1]+nums[n-2] < 0 {
 			continue
 		}
 
 		target := -1 * nums[l]
 		r := n - 1
 
-		for m := l + 1; m < n; m++ {
+		for m := l + 1; m < r; m++ {
 			// skip repeat
 			if m > l+1 && nums[m] == nums[m-1] {
 				continue
 			}
+
+			if nums[l]+nums[m]+nums[m+1] > 0 {
+				break
+			}
+
 			// reduce range
 			for m < r && nums[m]+nums[r] > target {
 				r--
